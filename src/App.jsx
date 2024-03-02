83934af74dd6780806b9c8f49f64b1438080c8d7
Ahmed Abd El-Ghany <ahmedabdoelhewala1211@gmail.com>
2024-03-02 06:27:22 +0200

    Update features

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,32 @@
+import { Suspense, lazy } from "react";
 import { ThemeModeProvider } from "./context/ThemeModeContext";
 import { ThemeColorProvider } from "./context/ThemeColorContext";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Projects from "./pages/Projects";
-import ErrorPage from "./pages/ErrorPage";
-import AppLayout from "./ui/AppLayout";
-import { useEffect, useState } from "react";
-import Spinner from "./ui/Spinner";
-import Skills from "./pages/Skills";
+const Home = lazy(() => import("./pages/Home"));
+const ErrorPage = lazy(() => import("./pages/ErrorPage"));
+const About = lazy(() => import("./pages/About"));
+const Projects = lazy(() => import("./pages/Projects"));
+const Skills = lazy(() => import("./pages/Skills"));
+const AppLayout = lazy(() => import("./ui/AppLayout"));
+const Spinner = lazy(() => import("./ui/Spinner"));
 
 function App() {
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    const stopLoading = () => {
-      setTimeout(() => {
-        setIsLoading(false);
-      }, 2000);
-    };
-    window.addEventListener("load", stopLoading);
-
-    return () => window.removeEventListener("load", stopLoading);
-  }, []);
-
   return (
     <ThemeModeProvider>
       <ThemeColorProvider>
-        {isLoading && <Spinner />}
         <BrowserRouter>
-          <AppLayout>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="about" element={<About />} />
-              <Route path="skills" element={<Skills />} />
-              <Route path="projects" element={<Projects />} />
-              <Route path="*" element={<ErrorPage />} />
-            </Routes>
-          </AppLayout>
+          <Suspense fallback={<Spinner />}>
+            <AppLayout>
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="about" element={<About />} />
+                <Route path="skills" element={<Skills />} />
+                <Route path="projects" element={<Projects />} />
+                <Route path="*" element={<ErrorPage />} />
+              </Routes>
+            </AppLayout>
+          </Suspense>
         </BrowserRouter>
       </ThemeColorProvider>
     </ThemeModeProvider>
--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,9 +1,23 @@
-
+import { useNavigate } from "react-router-dom";
+import styles from "./ErrorPage.module.css";
+import { HiOutlineArrowSmLeft, HiOutlineExclamation } from "react-icons/hi";
 
 function ErrorPage() {
+  const navigate = useNavigate();
+
   return (
-    <div>ErrorPage</div>
-  )
+    <div className={styles.errorPageContainer}>
+      <HiOutlineExclamation className={styles.exclamationSvg} />
+      <p className={styles.errorMsg}>
+        Oh, There is an error in reaching this page
+      </p>
+      <p className={styles.errorMsg}>Please! Go Back and try again 😃</p>
+      <button className={styles.errorBtn} onClick={() => navigate("/")}>
+        <HiOutlineArrowSmLeft />
+        <span>Back</span>
+      </button>
+    </div>
+  );
 }
 
-export default ErrorPage
+export default ErrorPage;
