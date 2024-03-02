d11466796aa34134e85e92ba2ed4e22eca750e62
Ahmed Abd El-Ghany <ahmedabdoelhewala1211@gmail.com>
2024-03-02 06:34:50 +0200

    Update featurs

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,22 +1,25 @@
 import { useNavigate } from "react-router-dom";
 import styles from "./ErrorPage.module.css";
 import { HiOutlineArrowSmLeft, HiOutlineExclamation } from "react-icons/hi";
+import Container from "../ui/Container";
 
 function ErrorPage() {
   const navigate = useNavigate();
 
   return (
-    <div className={styles.errorPageContainer}>
-      <HiOutlineExclamation className={styles.exclamationSvg} />
-      <p className={styles.errorMsg}>
-        Oh, There is an error in reaching this page
-      </p>
-      <p className={styles.errorMsg}>Please! Go Back and try again 😃</p>
-      <button className={styles.errorBtn} onClick={() => navigate("/")}>
-        <HiOutlineArrowSmLeft />
-        <span>Back</span>
-      </button>
-    </div>
+    <Container>
+      <div className={styles.errorPageContainer}>
+        <HiOutlineExclamation className={styles.exclamationSvg} />
+        <p className={styles.errorMsg}>
+          Oh, There is an error in reaching this page
+        </p>
+        <p className={styles.errorMsg}>Please! Go Back and try again 😃</p>
+        <button className={styles.errorBtn} onClick={() => navigate("/")}>
+          <HiOutlineArrowSmLeft />
+          <span>Back</span>
+        </button>
+      </div>
+    </Container>
   );
 }
 
